b18abae640f7fa67a0d4a3997739b6f972af8f2b
coradoo <conrado.matheus@protonmail.com>
2023-11-23 13:08:52 -0300

    commented in library.js

--- src/scripts/library.js
+++ src/scripts/library.js
@@ -1,22 +1,28 @@
 class Library {
+	// cria a classe biblioteca
 	constructor(collection, users) {
+		// cria o construtor da classe biblioteca
 		this.collection = collection;
 		this.users = users;
 	}
 
 	addItem(item) {
+		// metodo que adiciona um item a coleção
 		this.collection.push(item);
 	}
 
 	listCollection() {
+		// metodo que lista a coleção
 		console.log(this.collection);
 	}
 
 	addUser(user) {
+		// metodo que adiciona um usuario a coleção
 		this.users.push(user);
 	}
 
 	borrowItem(code, user) {
+		// metodo que empresta um item da coleção
 		let item = this.collection.find((item) => item.code === code);
 		if (item) {
 			item.borrowBook(user);
@@ -26,6 +32,7 @@ class Library {
 	}
 
 	returnItem(code) {
+		// metodo que devolve um item da coleção
 		let item = this.collection.find((item) => item.code === code);
 		if (item) {
 			item.returnBook();
@@ -36,18 +43,23 @@ class Library {
 }
 
 class User extends Library {
+	// cria a classe usuario que herda da classe biblioteca
 	constructor(name, academicRegister, birthDate) {
+		// cria o construtor da classe usuario
 		this.name = name;
 		this.academicRegister = academicRegister;
-		this.birthDate = new Date(birthDate);
+		this.birthDate = new Date(birthDate); // Converte a data de nascimento para o tipo Date
 	}
 	getBirthDate() {
+		// metodo que retorna a data de nascimento
 		return this.birthDate.toISOString().split("T")[0]; // Retorna a data no formato YYYY-MM-DD
 	}
 }
 
 class Enum extends Library {
+	// cria a classe enum que herda da classe biblioteca
 	constructor(suspense, romance, drama, horror, action, comedy) {
+		// cria o construtor da classe enum com os generos
 		this.suspense = suspense;
 		this.romance = romance;
 		this.drama = drama;
